b548040dc21edf8859649bb9b612494a29c13b3c
baltazar119 <abdulgazisimsek119@gmail.com>
2025-06-14 19:19:05 +0300

    Switch to .eslintrc.js and disable strict rules

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["next/core-web-vitals", "next"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-unused-expressions": "off",
+  },
+}
